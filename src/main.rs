ff4121a4d840f7d17d854f4fa70af9f1dccc7580
za_rudeboy <rudyada@gmail.com>
2019-05-16 19:49:30 +0200

    Added quote test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 fn count_characters(s: String) {
-    print!("{text} has {length} characters\n", text = s, length = s.len())
+    print!(
+        "{text} has {length} characters\n",
+        text = s,
+        length = s.len()
+    )
 }
 
-
-
-
+fn print_quotes(quote: String) {
+    let mut obi_said: String = "Obi shit head said \"".to_owned();
+    let closing_quote_mark = "\"";
+    obi_said.push_str(&quote);
+    obi_said.push_str(&closing_quote_mark);
+    println!("{}", obi_said);
+}
 
 #[cfg(test)]
 mod tests {
     use crate::count_characters;
+    use crate::print_quotes;
 
     #[test]
-    fn blah() {
+    fn should_count_characters() {
         count_characters(String::from("Homer"))
+    }
+
+    #[test]
+    fn should_print_quote() {
+        print_quotes(String::from("These aren't the droids you're looking for"))
     }
 }
